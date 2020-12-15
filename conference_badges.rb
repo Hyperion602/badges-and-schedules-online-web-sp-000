b1eb6c84f304ce2995f5391dcf768d633221ad6c
James Harris <mharris9608@yahoo.com>
2020-12-15 15:59:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  arr = []
+  names.each {|names2| names2 << arr}
+  arr
+end
+  
